ce40eb5668fd831c4a5839f9d7b67c5108e18516
marentius <vetlenilseen@hotmail.com>
2024-06-14 13:41:36 +0200

    test

---
+++ Oblig5_TVSHOWS_VetleMarentiusNilsen/vue-project/src/components/hello-world.vue
@@ -0,0 +1,14 @@
+<template id="hello-world">
+  <div>
+    <h1 class="hello-world">Hello, World!</h1>
+    <a href="/tvserie/">Go to Tvseries</a>
+  </div>
+</template>
+<script>
+app.component("hello-world", {template: "#hello-world"});
+</script>
+<style>
+.hello-world {
+  color: goldenrod;
+}
+</style>
